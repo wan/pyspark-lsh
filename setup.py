841a824d95afad13b9e33fec7c1f94711053aa55
Andrew Wan <andrewwan1@gmail.com>
2016-03-16 15:49:11 -0700

    moved lsh into module folder, added driver as script

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
         description='Locality-sensitive hashing for Apache Spark. Largely a PySpark port of the spark-hash project.',
         url='https://github.com/wan/pyspark-lsh',#this is forked; original is at https://github.com/magsol/pyspark-lsh
         packages=find_packages(),
+        scripts=['driver.py'],
         license='Apache Software License',
         install_requires=['numpy']#technically also pyspark, but that is not a pip dependency
 )
