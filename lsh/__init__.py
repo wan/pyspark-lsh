48b42e489e5122d9626b874843287bac7bc04de7
Andrew Wan <andrewwan@berkeley.edu>
2016-08-31 00:20:07 -0700

    revised run to avoid shuffle, modified docs

--- lsh/__init__.py
+++ lsh/__init__.py
@@ -1,11 +1,22 @@
-import hasher
+from collections import defaultdict
 import functools
-import numpy as np
 
+import numpy as np
 from pyspark.mllib.linalg import SparseVector
 
+import hasher
 
-def run(zdata, p, m, n, b, c):
+
+
+
+
+
+def run(zdata,
+        vector_size,
+        num_bins,
+        signature_size,
+        num_bands,
+        min_cluster_size):
     """
     Starts the main LSH process.
 
@@ -14,38 +25,32 @@ def run(zdata, p, m, n, b, c):
     zdata : RDD[Vector, id]
         RDD of data points. Acceptable vector types are numpy.ndarray
         or PySpark SparseVector.
-    p : integer, larger than the largest value in data.
-    m : integer, number of bins for hashing.
-    n : integer, number of rows to split the signatures into.
-    b : integer, number of bands.
-    c : integer, minimum allowable cluster size.
+    vector_size : integer, Prime number larger than the largest value in data.
+    num_bins : integer, number of bins for hashing.
+    signature_size : integer, number of rows to split the signatures into.
+    num_bands : integer, number of bands.
+    min_cluster_size : integer, minimum allowable cluster size.
     """
-    seeds = np.vstack([np.random.random_integers(p, size = n), np.random.random_integers(0, p, size = n)]).T
-    hashes = [functools.partial(hasher.minhash, a = s[0], b = s[1], p = p, m = m) for s in seeds]
+    seeds = np.vstack([np.random.random_integers(vector_size, size = signature_size), np.random.random_integers(0, vector_size, size = signature_size)]).T
+    hashers = [functools.partial(hasher.minhash, a = seed[0], b = seed[1], p = vector_size, m = num_bins) for seed in seeds]
 
-    # Start by generating the signatures for each data point.
-    # Output format is:
-    # <(vector id, band idx), minhash>
-    sigs = zdata.flatMap(lambda x: [[(x[1], i % b), hashes[i](x[0])] for i, h in enumerate(hashes)])
+    def hash_data(data, _id):
+        # Hash vector data into bands
+        hash_vals = defaultdict(set)
+        for band, hash_func in enumerate(hashers):
+            hash_vals[band % num_bands].add(hash_func(data))
+        return [((band, hash(frozenset(vals))), [_id])
+                  for band, vals in hash_vals.iteritems()]
 
-    # Put together the vector minhashes in the same band.
-    # Output format is:
-    # <(band idx, minhash list), vector idx>
-    bands = sigs.combineByKey(lambda vector: frozenset([vector]),
-            lambda vectors, vector: vectors.union(frozenset([vector])),
-            lambda left, right: left.union(right)) \
-        .map(lambda ((v_idx, b_idx), v_set): ((b_idx, hash(v_set)), v_idx)) \
-        .combineByKey(lambda v_set: [v_set],
-            lambda v_sets, v_set: v_sets + [v_set],
-            lambda left, right: left + right)
-
-    if c > 0:
-        bands = bands.filter(lambda (bucket, vectors): len(vectors) >= c)
+    sigs = zdata.flatMap(lambda (data, _id): hash_data(data, _id))
+    bands = sigs.reduceByKey(lambda x,y: x+y)
+    if min_cluster_size:
+        bands = bands.filter(lambda (bucket, vectors): len(vectors) >= min_cluster_size)
 
     # Remaps each element to a cluster / bucket index.
     # Output format is:
     # <vector id, bucket idx>
-    vector_bucket = bands.map(lambda (bucket, vectors): frozenset(sorted(vectors))) \
+    vector_bucket = bands.map(lambda (bucket, vectors): frozenset(vectors)) \
         .distinct() \
         .zipWithIndex() \
         .flatMap(lambda (vectors, idx): [(v, idx) for v in vectors])
--- lsh/hasher.py
+++ lsh/hasher.py
@@ -2,13 +2,15 @@ import numpy as np
 
 from pyspark.mllib.linalg import SparseVector
 
+
+
 def minhash(v, a, b, p, m):
     """
     Determines the type and computes the minhash of the vector.
         1: Multiplies the index by the non-zero seed "a".
         2: Adds the bias "b" (can be 0).
         3: Modulo "p", a number larger than the number of elements.
-        4: Modulo "m", the number of buckets.
+        4: Modulo "m", the number of bins.
 
     Parameters
     ----------
@@ -19,22 +21,22 @@ def minhash(v, a, b, p, m):
     b : integer
         Seed, >= 0.
     p : integer
-        Only restriction is that this number is larger than the number of elements.
+        Prime number that is larger than the number of elements.
     m : integer
         Number of bins.
 
     Returns
     -------
     i : integer
-        Integer minhash value that is in [0, buckets).
+        Integer minhash value that is in [0, bins).
     """
+
     indices = None
-    if type(v) is SparseVector:
+    if isinstance(v, SparseVector):
         indices = v.indices
-    elif type(v) is np.ndarray or type(v) is list:
+    elif isinstance(v, (np.ndarray, list)):
         indices = np.arange(len(v), dtype = np.int)
     else:
         raise Exception("Unknown array type '%s'." % type(v))
-
     # Map the indices to hash values and take the minimum.
     return np.array((((a * indices) + b) % p) % m).min()
